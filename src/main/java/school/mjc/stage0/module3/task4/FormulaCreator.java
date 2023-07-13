44245e7c38e7df7dd9094d08e0785a05a79e3a12
zfusername <farrukh_ziyaev@epam.com>
2023-07-13 17:11:50 +0500

    changed

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 1, b = 10;
+        double formula = (9 * Math.pow(a,2) - 5 * b + 2 + a - 7)*((a + b - 4 * a * b) / 2);
+
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int[] chars = {99, 104, 97, 114};
+
+        for (int charCode : chars) {
+            System.out.println((char) charCode);
+        }
+
     }
+
 }
